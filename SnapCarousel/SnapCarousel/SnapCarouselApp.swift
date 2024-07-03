d39aefb623930ffd50b8574d0ac95aaeb5fd25c5
Miriam Mendes <miriam.rayanemendes@ufpe.br>
2024-07-03 10:37:11 -0300

    add arquivo

---
+++ SnapCarousel/SnapCarousel/SnapCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCarouselApp.swift
+//  SnapCarousel
+//
+//  Created by Miriam Mendes on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
